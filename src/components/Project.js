4cc55c8910cd8d4185b697e14979c0d1e3985714
Amy <wilford.amy@gmail.com>
2022-12-12 22:51:24 -0500

    adjusted project text

--- src/components/Project.js
+++ src/components/Project.js
@@ -9,15 +9,15 @@ export default function Project({ projects }) {
           <div>
           <div key={project.id} className="row p-2">
             <div className="col-md-6"><img className="project-image img-fluid" src={project.src} alt={project.altTag}></img></div>
-            <div className="col-md-6 project-details ">
+            <div className="col-md-6 project-details">
               <h5 className="text-lowercase font-weight-bold projectName">
                 {project.name}
               </h5>
               <p className="m-0" >{project.stack}</p>
-              <p className="font-weight-light text-wrap pl-2 m-0">{project.details}</p>
+              <p className="font-weight-light text-wrap m-0">{project.details}</p>
               <p>{project.specs}</p>
               <div className="project-icon-row">
-                <div className="px-3">
+                <div className="pr-5">
                   <a
                     href={project.deployedUrl}
                     target="_blank"
@@ -26,7 +26,7 @@ export default function Project({ projects }) {
                     <i className="fa fa-2x fa-laptop project-icon"></i>
                   </a>
                 </div>
-                <div className="px-3">
+                <div className="pr-5">
                   <a href={project.githubRepo} target="_blank" rel="noreferrer">
                     <i
                       className="fa fa-2x fa-github project-icon"
