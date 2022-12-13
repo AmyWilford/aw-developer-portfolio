7eb1071d8f93bb1ce7763bda6df6f878db87f5f6
Amy <wilford.amy@gmail.com>
2022-12-12 22:20:04 -0500

    adjusted project page for accessibility

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 // import logo from "./logo.svg";
-import "./App.css";
 import Container from "./components/Container";
 
 function App() {
--- src/components/Project.js
+++ src/components/Project.js
@@ -6,47 +6,41 @@ export default function Project({ projects }) {
       <h4 className="mb-4 project-name">projects:</h4>
       <div className="project-container custom-margin">
         {projects.map((project) => (
-          <div key={project.id} className="image-container">
-            <img
-              className="project-image"
-              src={project.src}
-              alt={project.altTag}
-            />
-            <div className="overlay text-center">
-              <div className="overlay-text p-1">
-                <h5 className="text-lowercase font-weight-bold">
-                  {project.name}
-                </h5>
-                <p className="font-weight-light">{project.details}</p>
-                <p>{project.specs}</p>
-                <div className="row">
-                  <div className="col-6 text-right pr-3">
-                    <a
-                      href={project.deployedUrl}
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      <i className="fa fa-2x fa-laptop project-icon"></i>
-                    </a>
-                  </div>
-                  <div className="col-6 text-left pl-3">
-                    <a
-                      href={project.githubRepo}
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      <i
-                        className="fa fa-2x fa-github project-icon"
-                        aria-hidden="true"
-                      ></i>
-                    </a>
-                  </div>
+          <div key={project.id} className="row">
+            <div className="col-md-6 pb-2"><img className="project-image img-fluid" src={project.src} alt={project.altTag}></img></div>
+            <div className="col-md-6 project-details">
+              <h5 className="text-lowercase font-weight-bold projectName">
+                {project.name}
+              </h5>
+              <p className="m-0" >{project.stack}</p>
+              <p className="font-weight-light text-wrap pl-2 m-0">{project.details}</p>
+              <p>{project.specs}</p>
+              <div className="project-icon-row">
+                <div className="p-3">
+                  <a
+                    href={project.deployedUrl}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <i className="fa fa-2x fa-laptop project-icon"></i>
+                  </a>
+                </div>
+                <div className="p-3">
+                  <a href={project.githubRepo} target="_blank" rel="noreferrer">
+                    <i
+                      className="fa fa-2x fa-github project-icon"
+                      aria-hidden="true"
+                    ></i>
+                  </a>
                 </div>
               </div>
             </div>
+            <hr></hr>
+
           </div>
         ))}
       </div>
     </div>
   );
 }
+
